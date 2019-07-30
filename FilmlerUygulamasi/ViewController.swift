717e4a00975bf3f04ace43c0ca00da52e0994dc8
Kasim ADALAN <kasimadalan@gmail.com>
2019-07-30 13:50:08 +0300

    Initial Commit

---
+++ FilmlerUygulamasi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilmlerUygulamasi
+//
+//  Created by Kasım Adalan on 30.07.2019.
+//  Copyright © 2019 info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
